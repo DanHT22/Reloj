c6bf7d8e485866c474d8392656ba554999b089a7
DanHT22 <tolumes.daniela@gmail.com>
2023-07-12 18:37:22 -0600

    realice cambio

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ function hora(){
 }
 var horaActualizada = Date.parse(horaActualizada);//toLocaleTimeString(horaActualizada(setInterval(hora, 3000)));
 
-
 var miReloj = document.getElementById('reloj')
 miReloj.innerHTML=horaActualizada;
 console.log(typeof(horaActualizada));
